10c60d994fad895dfc5587d07bff2717f191714f
Kovats Kornel <kornel.kovats@idomsoft.hu>
2021-06-20 19:50:47 +0200

    feat: added basic template to NumberConverter.vue and constants

--- js-numerals/src/components/NumberConverter.vue
+++ js-numerals/src/components/NumberConverter.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>{{ msg }}</div>
+  <section>
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="number" placeholder="Enter your number here"/>
+    <button @click="convertToWord">Convert</button>
+    <p>{{ numberInWord }}</p>
+  </section>
 </template>
 
 <script>
+import * as constants from '../utils/constants';
 
 export default {
   name: 'NumberConverter',
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      number: null,
+      numberInWord: '',
+      constants,
+    };
+  },
+  methods: {
+    convertToWord() {
+      this.numberInWord = this.number;
+    },
   },
 };
 </script>
