cf9a2495ef088204d6fd77d603891ad0fa5bb32f
Kovats Kornel <kornel.kovats@idomsoft.hu>
2021-06-20 20:35:19 +0200

    style: added basic style for App.vue and NumberConverter.vue

--- js-numerals/src/components/NumberConverter.vue
+++ js-numerals/src/components/NumberConverter.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <input type="text" v-model="number" placeholder="Enter your number here"/>
     <button @click="convertToWord">Convert</button>
-    <p>{{ numberInWord }}</p>
+    <p>{{ "Your number in words: " + numberInWord }}</p>
   </section>
 </template>
 
@@ -30,5 +30,22 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+section {
+  font-family: 'Courier New', Courier, monospace;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+button{
+  color: #494949 !important;
+  text-transform: uppercase;
+  text-decoration: none;
+  background: #ffffff;
+  padding: 20px;
+  margin: 20px;
+  border: 4px solid #494949 !important;
+  display: inline-block;
+  transition: all 0.4s ease 0s;
+}
 </style>
