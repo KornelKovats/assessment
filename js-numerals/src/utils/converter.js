607a99fc865bc09f8ffe2b5060bd0dc786a4c574
Kovats Kornel <kovats.kornel95@gmail.com>
2021-06-20 23:48:07 +0200

    chore: trying to make the convertion with recursion

--- js-numerals/src/utils/converter.js
+++ js-numerals/src/utils/converter.js
@@ -2,11 +2,24 @@ import { constants } from './constants';
 
 // eslint-disable-next-line import/prefer-default-export
 export const numberConverterToWord = (number) => {
-  let word = '';
-  constants.forEach((element) => {
-    if (number === element.number) {
-      word += element.txt;
-    }
-  });
+  const word = '';
+  // eslint-disable-next-line no-use-before-define
+  convertRecursion(number, word);
   return word;
 };
+function convertRecursion(number, wordNeedsExtension) {
+  let newWord = wordNeedsExtension;
+  let underNumber;
+  constants.forEach((element) => {
+    if (number === element.number) {
+      newWord += `${element.txt} ${wordNeedsExtension}`;
+    }
+    if (element.number / number < 1 && number !== element.number) {
+      underNumber = element.number;
+    }
+    if (element.number / number > 1 && number !== element.number) {
+      convertRecursion(Math.floor(number / underNumber), newWord);
+    }
+  });
+  return newWord;
+}
