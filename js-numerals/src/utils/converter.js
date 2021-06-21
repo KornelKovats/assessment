6a663e9b66943c49b97280b3b4dc6411c88a039e
Kovats Kornel <kovats.kornel95@gmail.com>
2021-06-21 16:56:20 +0200

    chore: function works without the proper and values

--- js-numerals/src/utils/converter.js
+++ js-numerals/src/utils/converter.js
@@ -2,22 +2,40 @@
 import { constants } from './constants';
 
 // eslint-disable-next-line import/prefer-default-export
-export const numberConverterToWord = (number, wordNeedsExtension) => {
-  let newWord = wordNeedsExtension;
+export const numberConverterToWord = (number) => {
+  let newWord = '';
   let underNumber = constants[0].number;
-  const ifThereisWord = wordNeedsExtension || 0;
 
   for (let i = 0; i < constants.length; i += 1) {
     if (number === constants[i].number) {
-      newWord += `${constants[i].txt} ${ifThereisWord}`;
+      newWord = `${constants[i].txt}`;
       break;
     }
     if (constants[i].number / number < 1 && number !== constants[i].number) {
       underNumber = constants[i].number;
     }
-    if (constants[i].number / number > 1 && number !== constants[i].number) {
-      newWord = `${numberConverterToWord(Math.floor(number / underNumber))} ${numberConverterToWord(number / Math.floor(number / underNumber))}`;
+    if (constants[i].number / number > 1 && number !== constants[i].number
+        && betweenTwentyAndHoundred(number)) {
+      newWord = `${numberConverterToWord(underNumber)}-${numberConverterToWord(number - underNumber)}`;
+    }
+    if (constants[i].number / number > 1 && number !== constants[i].number
+         && greaterThanHoundred(number)) {
+      if (getRemainder(number, underNumber) === 0) {
+        newWord = `${numberConverterToWord(Math.floor(number / underNumber))} ${numberConverterToWord(underNumber)}`;
+      } else {
+        newWord = `${numberConverterToWord(Math.floor(number / underNumber))} ${numberConverterToWord(underNumber)} ${numberConverterToWord(getRemainder(number, underNumber))}`;
+      }
     }
   }
   return newWord;
 };
+
+function greaterThanHoundred(number) {
+  return number > 99;
+}
+function betweenTwentyAndHoundred(number) {
+  return number > 20 && number < 100;
+}
+function getRemainder(number, underNumber) {
+  return number - (Math.floor(number / underNumber) * underNumber);
+}
